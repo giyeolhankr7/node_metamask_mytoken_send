6a67cbec9582df56ee7041cbe33a28fe7c35f99d
giyeolhankr7 <72843832+giyeolhankr7@users.noreply.github.com>
2021-09-10 15:02:24 +0900

    Add files via upload

---
+++ public/js/sototoken.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    const ContractAddress = '0x85Eea33b46549e3a9dCB84e86cb69CE8be319F96';
+    // const ContractABI = $.getScript("/js/contractabi.json");
+
+    $('#linkHome').click(function () { showView("viewHome") });
+    $('#linkSubmitItem').click(function () { showView("viewSubmitItem"); });
+
+    $('#account').val(ContractAddress);
+    $('#contractLink').text(ContractAddress);
+    $('#contractLink').attr('href', 'https://ropsten.etherscan.io/address/' + ContractAddress);
+
+    // Attach AJAX "loading" event listener
+    $(document).on({
+        ajaxStart: function () { $("#loadingBox").show() },
+        ajaxStop: function () { $("#loadingBox").hide() }
+    });
+
+    function showView(viewName) {
+        // Hide all views and show the selected view only
+        $('main > section').hide();
+        $('#' + viewName).show();
+    }
+
+    function showInfo(message) {
+        $('#infoBox>p').html(message);
+        $('#infoBox').show();
+        $('#infoBox>header').click(function () { $('#infoBox').hide(); });
+    }
+
+    function showError(errorMsg) {
+        $('#errorBox>p').html("Error: " + errorMsg);
+        $('#errorBox').show();
+        $('#errorBox>header').click(function () { $('#errorBox').hide(); });
+    }
+
+});
+
